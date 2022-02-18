6529cbe6ad396aa916a9b4d9701f72c54d2d6e27
bisper1 <bisper@naver.com>
2022-02-18 17:24:17 +0900

    1. 리액트 기본 파일 2. app2.js 파일 추가 3. 내용 수정.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import {Fragment} from 'react'
 
 function App() {
+  // const name='리액트';
+  // const number = 0;
+
+  // return (
+  //   // <div className="App">
+  //   //   <header className="App-header">
+  //   //     <img src={logo} className="App-logo" alt="logo" />
+  //   //     <p>
+  //   //       Edit <code>src/App.js</code> and save to reload.
+  //   //     </p>
+  //   //     <a
+  //   //       className="App-link"
+  //   //       href="https://reactjs.org"
+  //   //       target="_blank"
+  //   //       rel="noopener noreferrer"
+  //   //     >
+  //   //       Learn React
+  //   //     </a>
+  //   //   </header>
+  //   // </div>
+
+  //   // <div>
+  //   //   <h1>리액트 안녕!</h1>
+  //   //   <h2>잘 작동하니?</h2>
+  //   // </div>
+
+  //   // <Fragment>
+  //   //   <h1>리액트 안녕</h1>
+  //   //   <h2>이건 잘 되나??</h2>
+  //   // </Fragment>
+
+  //   // <div>
+  //   //   { name === '리액트' ? (
+  //   //     <h1>리액트 입니다.</h1>
+  //   //   ) : (
+  //   //     <h1>리액트가 아닙니다.</h1>
+  //   //   )}
+  //   // </div>
+
+  //   // <div>{ name === '리액트' ? <h1>리액트 입니다.</h1> : null}</div>
+
+  //   <div>
+  //     {
+  //       name === '리액트' && <h1>리액트 입니다.</h1>
+  //     }
+  //     {
+  //       number !== 0 && <h2>이건 나오나</h2>
+  //     }
+  //   </div>
+  // );
+
+  // const name = undefined;
+  // return name || '값이 undefined 입니다.';
+
+  // const name = undefined;
+  // return <div>{name}</div>;
+
+  // const name = undefined;
+  // return <div>{name || '리액트'}</div>;
+
+  const name = '리액트';
+  const style = {
+    backgroundColor:'black',
+    color:'aqua',
+    fontSize:'48px',
+    fontWeight:'bold',
+    padding:16
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <div style={style}>{name}</div>
+        <div style={{
+          backgroundColor: 'black',
+          color: 'aqua',
+          fontSize: '48px',
+          fontWeight: 'bold',
+          padding: 16
+        }}>{name}</div>
+        <div className='react'>{name}</div>
+
+        <div className='react'>{name}</div>
+        <input></input>
+        {/* 이것이 주석 */}
+        {/* 
+        // 
+        // 이렇게도 사용 가능
+        // 
+        */}
+      </div>
+    );
 }
 
 export default App;
---
+++ src/App2.js
@@ -0,0 +1,15 @@
+// import './App2.css';
+
+function App2() {
+  const name = '리액트';
+  return (
+    <div>
+      <div className="react">{name}</div>
+      <h1>들여쓰기가 이상한</h1>
+      <h2>코드</h2>
+      <p>입니다.</p>
+    </div>
+  );
+}
+
+export default App2;
